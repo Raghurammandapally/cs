4ebfe087bb34e1741d1b43db32029baa4b92d132
Keith Funkhouser <keith.funkhouser@gmail.com>
2015-09-08 20:58:49 -0500

    changed to linkedlist

--- cs536/p1/SymTable.java
+++ cs536/p1/SymTable.java
@@ -5,12 +5,13 @@ import java.util.*;
  * */
 
 public class SymTable {
-  private List<HashMap<String,Sym>> tableList;
+  //is this allowed? see ^
+  private LinkedList<HashMap<String,Sym>> tableList;
 
   public SymTable() {
     //should initialize the SymTable's List field to contain a single, empty HashMap.
-    tableList = new ArrayList<HashMap<String,Sym>>();
-    tableList.add(new HashMap<String,Sym>());
+    tableList = new LinkedList<HashMap<String,Sym>>();
+    addScope();
   }
 
   public void addDecl(String name, Sym sym) throws DuplicateSymException, EmptySymTableException {
@@ -20,27 +21,27 @@ public class SymTable {
     } else if(name==null || sym==null) {
       // If either name or sym (or both) is null, throw a NullPointerException. 
       throw new NullPointerException();
-    } else if(tableList.get(0).containsKey(name)) {
+    } else if(tableList.getFirst().containsKey(name)) {
       // If the first HashMap in the list already contains the given name as a key, throw a DuplicateSymException. 
       throw new DuplicateSymException();
     } else {
       // Otherwise, add the given name and sym to the first HashMap in the list.
-      tableList.get(0).put(name, sym);
+      tableList.getFirst().put(name, sym);
     }
   }
 
   public void addScope() {
     // Add a new, empty HashMap to the front of the list.
-    tableList.add(0, new HashMap<String,Sym>());
+    tableList.addFirst(new HashMap<String,Sym>());
   }
 
   public Sym lookupLocal(String name) throws EmptySymTableException {
     if(tableList.isEmpty()) {
       //If this SymTable's list is empty, throw an EmptySymTableException. 
       throw new EmptySymTableException();
-    } else if (tableList.get(0).containsKey(name)) {
+    } else if (tableList.getFirst().containsKey(name)) {
       //Otherwise, if the first HashMap in the list contains name as a key, return the associated Sym; 
-      return tableList.get(0).get(name);
+      return tableList.getFirst().get(name);
     } else {
       //otherwise, return null.
       return null;
@@ -69,7 +70,7 @@ public class SymTable {
       throw new EmptySymTableException();
     } else {
       //otherwise, remove the HashMap from the front of the list. 
-      tableList.remove(0);
+      tableList.removeFirst();
     }
     //To clarify, throw an exception only if before attempting to remove, the list is empty (i.e. there are no HashMaps to remove).
   }
