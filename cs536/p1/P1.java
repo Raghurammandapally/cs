9caa40fde7075970657ebb891c0082a029fe8641
Keith Funkhouser <keith.funkhouser@gmail.com>
2015-09-11 11:25:40 -0500

    -

--- cs536/p1/P1.java
+++ cs536/p1/P1.java
@@ -3,33 +3,70 @@ import static java.lang.System.out;
 public class P1 {
   public static void main (String[] args) throws EmptySymTableException,DuplicateSymException {
     //can we test this using a try/catch block?
-    SymTable s = new SymTable();
     String err = "  ERROR!";
     String exc = "  THIS SHOULD HAVE FAILED!";
     boolean success = false;
 
-    out.println("Checking throwing an EmptySymTableException...");
-    try {
-      throw new EmptySymTableException();
-    } catch (Exception e) {
-      out.println(err);
+    out.println("*****************************************");
+    out.println("************Beginning tests**************");
+    out.println("*****************************************");
+
+    /* uncomment these to test throwing our exceptions 
+     *  note: you will also have to comment out all remaining code in order
+     *        not to receive "unreachable statement" errors on compilation 
+     *
+     * (leave lines commented in order to run rest of tests)
+    */
+    //throw new EmptySymTableException();
+    //throw new DuplicateSymException();
+
+    //initialize our SymTable
+    SymTable s = new SymTable();
+
+    //initialize an array of test Sym's
+    //  syms[0] = new Sym("val0");
+    //  syms[1] = new Sym("val1");
+    //      ...
+    //  syms[9] = new Sym("val9");
+    Sym[] syms = new Sym[10];
+    for (int i=0; i < 10; i++) {
+      syms[i] = new Sym("val" + i);
     }
 
-    out.println("Checking adding values to empty table...");
+    out.println("Checking adding multiple values to empty table...");
     try {
       for(int i=0; i < 10; i++) {
-        s.addDecl("key"+i, new Sym("val"+i));
+        s.addDecl("key"+i, syms[i]);
       }
     } catch (Exception e) {
       out.println(err);
     }
 
+    out.println("Checking retrieving those values locally...");
+    try {
+      for(int i=0; i < 10; i++) {
+        if(s.lookupLocal("key"+i) != syms[i]) {
+          out.println(err + "idx " + i);
+        }
+      }
+    } catch (Exception e) {
+      out.println(err);
+    }
+
+    out.println("Checking retrieving those values globally...");
+    for(int i=0; i < 10; i++) {
+      if(s.lookupGlobal("key"+i) != syms[i]) {
+        out.println(err + "idx " + i);
+      }
+    }
+
     out.println("Checking adding a duplicate value...");
     success = false;
     try {
-      s.addDecl("key0", new Sym("val0"));
+      s.addDecl("key0", new Sym("diffval"));
       success = true;
     } catch (DuplicateSymException e) {
+      //expected behavior
     } finally {
       if(success) {
         out.println(exc);
@@ -49,6 +86,7 @@ public class P1 {
       s.removeScope();
       success = true;
     } catch (EmptySymTableException e) {
+      //expected behavior
     } finally {
       if(success) {
         out.println(exc);
@@ -61,6 +99,7 @@ public class P1 {
       s.addDecl("key", new Sym("val"));
       success = true;
     } catch (EmptySymTableException e) {
+      //expected behavior
     } finally {
       if(success) {
         out.println(exc);
@@ -76,6 +115,7 @@ public class P1 {
       // do something that should throw an exception
       success = true;
     } catch (Exception e) {
+      //expected behavior
     } finally {
       if(success) {
         out.println(exc);
