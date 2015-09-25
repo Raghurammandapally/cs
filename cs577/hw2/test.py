4d976af2338e973188f9b1948d30ca060d9cebcc
William Funkhouser <wfunkhouser@macaroni-08.cs.wisc.edu>
2015-09-25 11:07:25 -0500

    -

--- cs577/hw2/test.py
+++ cs577/hw2/test.py
@@ -1,4 +1,6 @@
-import pdb
+import random
+import csv
+import time
 import math
 
 def FindMaxRect(A):
@@ -11,18 +13,17 @@ def FindMaxRectRec(A,start,end):
   mid = int(math.floor((start+end)/2))
   maxL = FindMaxRectRec(A,start,mid)
   maxR = FindMaxRectRec(A,mid+1,end)
-  pdb.set_trace()
   maxC = max(maxL,maxR)
   l = mid
   r = mid+1
   while(l>=start and r<=end):
     if(A[l]<A[r]):
       value = A[r]
-      while(r<=end and A[l]<=A[r]):
+      while(r<=end and value<=A[r]):
           r=r+1
     elif(A[l]>A[r]):
       value = A[l]
-      while(l>=start and A[l]>=A[r]):
+      while(l>=start and A[l]>=value):
           l=l-1
     else:
       value = A[r]
@@ -46,8 +47,39 @@ def FindMaxRectRec(A,start,end):
     r= r+1
     m = value*(r-l-1)
     maxC = max(maxC,m)
-  print("max of " + str(A) + " on [" + str(start) + ", " + str(end) + "] is " + str(maxC))
+  #print("max of " + str(A) + " on [" + str(start) + ", " + str(end) + "] is " + str(maxC))
   return maxC
+
+"""
+def FindMaxRectRec(A,start,end):
+  if(start==end):
+    return A[start]
+  mid = int(math.floor((start+end)/2))
+  maxL = FindMaxRectRec(A,start,mid)
+  maxR = FindMaxRectRec(A,mid+1,end)
+  maxC = max(maxL,maxR)
+  l = mid
+  r = mid+1
+  while(l>=start or r<=end):
+    if(r>end):
+      R = -1
+    else:
+      R = A[r]
+    if(l<start):
+      L = -1
+    else:
+      L = A[l]
+    value = max(L,R)
+    if(L<=R):
+      while(r<=end and A[r]>=value):
+        r = r+1
+    if(L>=R):
+      while(l>=start and A[l]>=value):
+        l = l-1
+    m = value*(r-l-1)
+    maxC=max(maxC,m)
+  return maxC
+"""
 
 def BruteForce(A):
   B = [0]*len(A)
@@ -62,14 +94,31 @@ def BruteForce(A):
   return max(B)
 
 def compare(A):
-  print("***************************")
-  print(A)
-  print("***************************")
-  haruki = FindMaxRect(A)
+  start = time.time()
+  dAndC = FindMaxRect(A)
+  end = time.time()
+  startBrute = time.time()
   brute = BruteForce(A)
-  print("Haruki: "+str(haruki))
-  print("Brute force: "+str(brute))
+  endBrute = time.time()
+  print("testing an array of length " + str(len(A)) + "...")
+  if(dAndC!=brute):
+    print("ERROR!")
+    print("  " + str(A))
+    print("  D&C: "+str(dAndC))
+    print("  Brute force: "+str(brute))
+  return [end-start,endBrute-startBrute]
 
-#compare([7,1,2,5,4,8,6,3])
+def test():
+  runtimes = []
+  for i in range(100):
+    n = random.randint(0,100000)
+    runtime = compare([random.randint(1,10000) for j in range(n)])
+    runtimes.append([n,runtime[0], runtime[1]])
 
-FindMaxRect([7,1,2,5,4,8,6,3])
+  with open('runtime.csv', 'wb') as csvfile:
+    for line in runtimes:
+        outwriter = csv.writer(csvfile)
+        outwriter.writerow(line)
+
+test()
+
