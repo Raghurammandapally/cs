df2d82dda230960e1456719958063d00b5075566
Keith Funkhouser <keith.funkhouser@gmail.com>
2016-02-10 11:47:47 -0600

    -

--- cs537/p2/whoosh.c
+++ cs537/p2/whoosh.c
@@ -2,14 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+// 128 for chars, 1 for \n and 1 for \0
+#define MAX_LEN 130
+
 
 
 int main() {
+  //the one and only error message:
   const char error_message[30] = "An error has occurred\n";
   //write(STDERR_FILENO, error_message, strlen(error_message));
+
   const char *greeting = "whoosh> ";
-write(STDERR_FILENO, error_message, strlen(error_message));
+
+  char input[MAX_LEN];
+
   printf("%s", greeting); 
+  fgets(input, MAX_LEN, stdin);
+
+  char *args[MAX_LEN-2];
+  printf("you wrote: %s\n", input);
+
+  args[0] = strtok(input, " ");
+  printf("first arg: %s\n", args[0]);
+
   exit(0);
 
 }
