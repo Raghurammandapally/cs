6572ff2d90c9ada96c577155c6d428c8c6125dc7
Keith Funkhouser <keith.funkhouser@gmail.com>
2016-02-10 08:10:20 -0600

    -

--- cs537/p2/whoosh.c
+++ cs537/p2/whoosh.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 
 int main() {
+  const char error_message[30] = "An error has occurred\n";
+  //write(STDERR_FILENO, error_message, strlen(error_message));
   const char *greeting = "whoosh> ";
+write(STDERR_FILENO, error_message, strlen(error_message));
   printf("%s", greeting); 
   exit(0);
 
