73c2694206cfd14b56b1905f41266c211fc368bb
Keith Funkhouser <keith.funkhouser@gmail.com>
2016-02-13 16:17:24 -0600

    working p2

--- cs537/p2/whoosh.c
+++ cs537/p2/whoosh.c
@@ -14,6 +14,10 @@ struct path {
   int count;
 };
 
+void writeout(char *str) {
+  write(STDOUT_FILENO, str, strlen(str));
+}
+
 void err() {  
   write(STDERR_FILENO, error_message, strlen(error_message)); 
 }
@@ -21,10 +25,10 @@ void err() {
 void clear_paths(struct path *p) {
   int i;
   //argc/argv are passed directly from the cli, so they include:
-  // argc[0]: path
-  // argc[1]: (first dir)
+  // argv[0]: path
+  // argv[1]: (first dir)
   // ...
-  // argc[argv-1]: (last dir)
+  // argv[argc-1]: (last dir)
 
   // free all old pointers
   for(i = 0; i < p->count; i++) {
@@ -36,25 +40,25 @@ void clear_paths(struct path *p) {
   }
 }
 
-int update_path(struct path *p, char **argc, int argv) {
+int update_path(struct path *p, char **argv, int argc) {
   clear_paths(p);
 
   int i;
   // set count var before potentially returning
-  p->count = argv-1;
+  p->count = argc-1;
 
-  if(argv == 1) {
+  if(argc == 1) {
     //no need to allocate space
     return 0;
   }
 
   // add all new *chars
-  p->paths = malloc((argv-1)*sizeof(char*));
+  p->paths = malloc((argc-1)*sizeof(char*));
   if(p->paths == NULL) {
     return 1;
   }
-  for(i = 1; i < argv; i++) {
-    p->paths[i-1] = strdup(argc[i]);
+  for(i = 1; i < argc; i++) {
+    p->paths[i-1] = strdup(argv[i]);
     if(p->paths[i-1] == NULL) {
       return 1;
     }
@@ -63,12 +67,6 @@ int update_path(struct path *p, char **argc, int argv) {
   return 0;
 }
 
-void print_paths(struct path *p) {
-  int i;
-  for(i = 0; i < p->count; i++) {
-    printf("path[%d]: %s\n", i, p->paths[i]);
-  }
-}
 
 int main() {
   //the one and only error message:
@@ -77,21 +75,22 @@ int main() {
 
   struct path p = {.count = 0, .paths = NULL};
 
-  const char *greeting = "whoosh> ";
+  char *greeting = "whoosh> ";
   char *init_paths[2];
   // has to start at 1 because update_paths starts there
   init_paths[1] = "/bin";
-  // have to pass 1+1 == 2 since we calculate argv-1 in update_path
+  // have to pass 1+1 == 2 since we calculate argc-1 in update_path
   update_path(&p, init_paths, 2);
 
-  int argv;
+  int argc;
   char input[MAX_LEN];
   while(1) {
 
-    printf("%s", greeting); 
+    writeout(greeting);
+    //printf("%s", greeting); 
     fgets(input, MAX_LEN, stdin);
 
-    char *args[MAX_LEN-2];
+    char *argv[MAX_LEN-2];
 
 
     //  PRINT ERROR MESSAGE AND EXIT:
@@ -105,6 +104,7 @@ int main() {
     //A very long command line (over 128 bytes).
     if (strlen(input) == MAX_LEN-1 && input[strlen(input)-2] != '\n') {
       err();
+      continue;
     }
 
   
@@ -131,49 +131,46 @@ int main() {
     // check all error states above
     // (MAX/2 + 1 since, if every other letter is a token and there are 128 allowed, then there can only be 64)
     // strtok -> *char[MAX_LENGTH/2]
-    args[0] = strtok(input, " ");
-    if(args[0] == NULL) {
+    argv[0] = strtok(input, " ");
+    if(argv[0] == NULL) {
       continue;
     }
 
-    argv = 1;
-    while((args[argv] = strtok(NULL, " ")) != NULL) {
-      argv++;
+    argc = 1;
+    while((argv[argc] = strtok(NULL, " ")) != NULL) {
+      argc++;
     }
-
-    // now argv == the # of args
-    printf("argv is now %d\n", argv);
-
-
 
     //So far, you have added your own exit built-in command. Most Unix shells have many others such as cd , echo , pwd , etc. In this project, you should implement exit, cd, pwd, and path.
 
     //The formats for exit, cd, and pwd are:
 
     //[optionalSpace]exit[optionalSpace]
-    if(argv == 1 && strcmp(args[0], "exit\n") == 0) {
+    if(argc == 1 && strcmp(argv[0], "exit\n") == 0) {
       exit(0);
-    } else if(strcmp(args[0], "exit") == 0) {
+    } else if(strcmp(argv[0], "exit") == 0) {
       err();
       exit(1);
     }
 
 
     //[optionalSpace]pwd[optionalSpace]
-    else if(argv == 1 && strcmp(args[0], "pwd\n") == 0) {
+    else if(argc == 1 && strcmp(argv[0], "pwd\n") == 0) {
       char *cwd;
       cwd = getcwd(NULL, 0);
-      printf("%s\n", cwd);
+      writeout(cwd);
+      writeout("\n");
+      //printf("%s\n", cwd);
       free(cwd);
-    } else if (strcmp(args[0], "pwd") == 0) {
+    } else if (strcmp(argv[0], "pwd") == 0) {
       err();
       exit(1);
     }
 
 
-    else if ((argv == 1 && strcmp(args[0], "cd\n") == 0) || 
-	     (argv > 1 && strcmp(args[0], "cd") == 0)) {
-      if(argv > 2) {
+    else if ((argc == 1 && strcmp(argv[0], "cd\n") == 0) || 
+	     (argc > 1 && strcmp(argv[0], "cd") == 0)) {
+      if(argc > 2) {
 	err();
 	exit(1);
       } else {
@@ -181,7 +178,7 @@ int main() {
 	int res;
 
 	//[optionalSpace]cd[optionalSpace]
-	if(argv == 1) {
+	if(argc == 1) {
 	  dir = getenv("HOME");
 	  if(dir == NULL) {
 	    err();
@@ -189,7 +186,7 @@ int main() {
 	  }
 	} else {
 	  //[optionalSpace]cd[oneOrMoreSpace]dir[optionalSpace]
-	  dir = args[1];
+	  dir = argv[1];
 	}
 
 	res = chdir(dir);
@@ -202,9 +199,9 @@ int main() {
       }
     }
     //[optionalSpace]path[oneOrMoreSpace]dir[optionalSpace] (and possibly
-    else if ((argv == 1 && strcmp(args[0], "path\n") == 0) || 
-	     (argv > 1 && strcmp(args[0], "path") == 0)) {
-      update_path(&p, args, argv);
+    else if ((argc == 1 && strcmp(argv[0], "path\n") == 0) || 
+	     (argc > 1 && strcmp(argv[0], "path") == 0)) {
+      update_path(&p, argv, argc);
       //print_paths(&p);
     }
 
@@ -213,11 +210,13 @@ int main() {
       //For the following situation, you should print the error message to stderr and continue processing:
       //  - A command does not exist or cannot be executed.
 
-      if(args[0][strlen(args[0])-1] == '\n') {
-	args[0][strlen(args[0])-1] = '\0';
-      }
+      //if(argv[0][strlen(argv[0])-1] == '\n') {
+      argv[argc-1][strlen(argv[argc-1])-1] = '\0';
+      //}
+
       int j;
       struct dirent *de;
+      int found = 0;
       for(j = 0; j < p.count; j++) {
 	DIR *dir = opendir(p.paths[j]);
 	if(dir == NULL) {
@@ -225,39 +224,44 @@ int main() {
 	} 
 	// statically allocated; no need to free()
 	while( (de = readdir(dir)) != NULL) {
-	  if( strcmp(args[0], de->d_name) == 0) {
+	  if( strcmp(argv[0], de->d_name) == 0) {
 	    // we have found our (potential) executable
+	    found = 1;
 	    int rc = fork();
 	    if(rc == 0) { // child process
-	      printf("child process!\n");
-	      printf("argv: %d\n", argv);
-	      char *my_args[argv+1];
+	      char *my_args[argc+1];
 
 	      // 1 for '/' between and 1 for \0
-	      my_args[0] = malloc(sizeof(char)*( strlen(p.paths[j]) + strlen(args[0]) + 2));
-	      memcpy((void *) *my_args[0], *p.paths[j], sizeof(char)*strlen(p.paths[j]));
-	      my_args[0][strlen(p.paths[j])] = '/';
-	      memcpy((void *) my_args[0][strlen(p.paths[j]+1)], *args[0], sizeof(char)*strlen(args[0]));
-	      my_args[strlen(p.paths[j]) + strlen(args[0]) + 1] = '\0';	      
+	      my_args[0] = malloc(sizeof(char)*( strlen(p.paths[j]) + strlen(argv[0]) + 2));
+
+	      strcpy(my_args[0], p.paths[j]);
+	      strcat(my_args[0], "/");
+	      strcat(my_args[0], argv[0]);
+
+	      my_args[argc] = NULL;
 
 	      int i;
-	      for(i = 1; i < argv; i++) {
-		my_args[i] = args[i];
+	      for(i = 1; i < argc; i++) {
+		my_args[i] = argv[i];
 	      }
-	      my_args[argv] = NULL;
-	      
+	      my_args[argc] = NULL;
+
 	      //execv: must be passed a **char with the last == NULL
 	      execv(my_args[0], my_args);
+	      //there has been an error in the execv call if this line executes
+	      err();
+	      break;
+	    } else if (rc > 0) { // parent process
+
+	    } else { // error with execv
 	      err();
 	      exit(1);
-	    } else if (rc > 0) { // parent process
-	      printf("parent process!\n");
-	      printf("argv: %d\n", argv);
-	    } else { // error
-
 	    }
-	    continue;
 	  }
+	}
+	//break out of for loop over paths
+	if(found) {
+	  break;
 	}
       }
     }
