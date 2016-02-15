1d5c5dd80c4a542fbb4ae027ffa6272a46bedf60
Keith Funkhouser <keith.funkhouser@gmail.com>
2016-02-15 10:14:52 -0600

    -

--- cs537/p2/whoosh.c
+++ cs537/p2/whoosh.c
@@ -1,4 +1,8 @@
+#include <sys/wait.h>
+#include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,7 +23,7 @@ void err() {
   write(STDERR_FILENO, error_message, strlen(error_message)); 
 }
 
-int main() {
+      int main() {
   struct path p = {.count = 0, .paths = NULL};
 
   char *greeting = "whoosh> ";
@@ -74,6 +78,27 @@ int main() {
       argv[ptr] = strtok(NULL, " ");
     }
 
+    int redir = 0;
+    int last_redir = 0;
+    for(ptr = 1; ptr < argc; ptr++) {
+      if(strcmp(argv[ptr], ">") == 0) {
+	redir++;
+	last_redir = ptr;
+      }
+    }
+    // do error checking for redirection here, but closing/opening of files
+    // in child process
+    if(redir > 1) {
+      err();
+      continue;
+    } else if (redir == 1) {
+      if(last_redir != argc-2) {
+	err();
+	continue;
+      } else {
+      }
+    }
+
     //these should be the correct params for execv now
     assert(argv[argc] == NULL);
 
@@ -83,10 +108,13 @@ int main() {
     //   -pwd
     //   -path
     if(strcmp(argv[0], "exit") == 0) {
+      if(argc>1) {
+	err();
+	exit(1);
+      }
       exit(0);
     } else if( strcmp(argv[0], "cd") == 0 ) {
       char *dir;
-
       if(argc > 2) {
 	err();
 	exit(1);
@@ -116,18 +144,83 @@ int main() {
       update_path(&p, argv, argc);
       //print_paths(&p);
     } else {
+      struct stat *buf;
+      buf = malloc(sizeof(struct stat));
+      int res;
+      //keep track of if the exec was found
+      int found = 0;
       //search for executables in path dirs
-      
+      for(ptr = 0; ptr < p.count; ptr++) {
+	//+1 for '/' in middle
+	int len = strlen(p.paths[ptr]) + strlen(argv[0]) + 1;
+	//+1 for \0 at end
+	char *dest = malloc( sizeof(char) * (len + 1));
+	strcpy(dest, p.paths[ptr]);
+	dest[strlen(p.paths[ptr])] = '/';
+	strcpy( dest + strlen(p.paths[ptr]) + 1 , argv[0]);
+
+	res = stat(dest, buf);
+	if(res != 0) {
+	  //err();
+	  free(dest);
+	  continue;
+	} else {
+	  argv[0] = dest;
+	  int rc = fork();
+	  if(rc == 0) { // child
+
+	    if(redir > 0) {
+	      //need to:
+	      // - close stdout and stderr
+	      // - open new stdout and stderr files
+	      char *prefix = argv[last_redir + 1];
+	      // 4 for ".out" and 1 for \0
+	      char *out = malloc(strlen(prefix) + 5);
+	      strcpy(out, prefix);
+	      strcat(out, ".out");
+	      char *errout = malloc(strlen(prefix) + 5);
+	      strcpy(errout, prefix);
+	      strcat(errout, ".err");
+
+	      close(STDOUT_FILENO);
+	      open(out,
+		   O_CREAT | O_TRUNC | O_WRONLY,
+		   S_IRUSR | S_IWUSR);
+
+	      close(STDERR_FILENO);
+	      open(errout,
+		   O_CREAT | O_TRUNC | O_WRONLY,
+		   S_IRUSR | S_IWUSR);
+
+	      // - set argv[redir] = NULL
+	      argv[redir] = NULL;
+	      // - set argc = redir
+	      argc = redir;
+	    }
+
+	    execv(argv[0], argv);
+	    // if these lines of code are executed, an error has occurred
+	    err();
+	    exit(1);
+	  } else if (rc > 0) { //parent
+	    int cpid = (int) wait(NULL);
+	    found = 1;
+	    break;
+	  } else { //error
+	    err();
+	    exit(1);
+	  }
+	}
+
+      }
+      if(!found) {
+	err();
+	continue;
+      }
     }
 
     // Clean up input argv
     free(argv);
-
-
-
-
-
-
 
   }
   //********************************
